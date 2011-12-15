9f7adfe8a25c674f127ef1aae5563074a8fb4a95
David Padilla <david@crowdint.com>
2011-12-15 01:00:35 -0600

    Added a Rails dependency that I am not really sure it's correct. (it's late)

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,4 @@
+require 'rails'
 require 'persistent_settings'
 
 ::ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'rails'
 require 'active_record'
 require 'persistent_settings'
 
