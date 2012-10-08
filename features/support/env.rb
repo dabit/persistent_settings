970901d7c6bd2b183c09f064129dca46e43f5946
David Padilla <david@crowdint.com>
2012-10-08 13:26:01 -0500

    Change test output to be more discreet

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,9 @@
 require 'rails'
 require 'persistent_settings'
+require 'logger'
 
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Base.logger = Logger.new("test.log")
 ::ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 ::ActiveRecord::Schema.define(:version => 1) do
   create_table :settings do |t|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'persistent_settings'
+require 'logger'
 
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Base.logger = Logger.new("test.log")
 ::ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
 ::ActiveRecord::Schema.define(:version => 1) do
