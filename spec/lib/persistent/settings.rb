fe97f73b32bfc0488db2776f22fbb5405788ae15
David Padilla <david@crowdint.com>
2012-10-10 09:40:51 -0500

    Use a dynamic class for testing

--- spec/lib/persistent/settings.rb
+++ spec/lib/persistent/settings.rb
@@ -1,70 +1,64 @@
 require 'spec_helper'
 
-describe Persistent::Settings do
-  before do
-    klass = Class.new ActiveRecord::Base
-    suppress_warnings { ::Config = klass }
-    ::Config.send :include, Persistent::Settings
-  end
-
+describe Settings do
   describe :assignation? do
     context "the method name ends with =" do
       it "returns true" do
-        Config.assignation?("method=").should be_true
+        Settings.assignation?("method=").should be_true
       end
     end
 
     context "the method name does not end with =" do
       it "returns false" do
-        Config.assignation?("method").should be_false
+        Settings.assignation?("method").should be_false
       end
     end
   end
 
   describe :define_setter_and_getter do
     before do
-      Config.define_setter_and_getter(:method_name=)
+      Settings.define_setter_and_getter(:method_name=)
     end
 
     it "creates the setter and getter methods" do
-      Config.should respond_to(:method_name=)
-      Config.should respond_to(:method_name)
+      Settings.should respond_to(:method_name=)
+      Settings.should respond_to(:method_name)
     end
 
     context :getter_method do
       context "value is persisted on cache" do
         before do
-          Config.should_receive(:read_from_cache).with('method_name').
+          Settings.should_receive(:read_from_cache).with('method_name').
               and_return('value from cache')
         end
 
         it "reads and returns value from cache" do
-          Config.method_name.should == 'value from cache'
+          Settings.method_name.should == 'value from cache'
         end
       end
 
       context "value is not persisted on cache" do
         before do
-          Config.should_receive(:read_from_cache).with('method_name')
+          Settings.should_receive(:read_from_cache).with('method_name')
         end
 
         it "reads and returns value from persistance, then saves to cache" do
-          Config.should_receive(:read_from_persistance).with('method_name').
+          Settings.should_receive(:read_from_persistance).with('method_name').
               and_return 'value from persistance'
 
-          Config.should_receive(:write_to_cache).
+          Settings.should_receive(:write_to_cache).
               with('method_name', 'value from persistance')
 
-          Config.method_name.should == 'value from persistance'
+          Settings.method_name.should == 'value from persistance'
         end
       end
     end
 
     context :setter_method do
       it "persists and writes to cache the key/value" do
-        Config.should_receive(:persist).with('method_name', 'value')
-        Config.should_receive(:write_to_cache).with('method_name', 'value')
-        Config.method_name = 'value'
+        Settings.should_receive(:persist).with('method_name', 'value')
+        Settings.should_receive(:write_to_cache).with('method_name', 'value')
+        Settings.method_name = 'value'
       end
     end
   end
@@ -72,37 +66,37 @@ describe Persistent::Settings do
   describe :persist do
     context "a value already persisted" do
       before :each do
-        @setting = mock(Config)
-        Config.stub_chain(:where, :last).and_return(@setting)
+        @setting = mock(Settings)
+        Settings.stub_chain(:where, :last).and_return(@setting)
       end
 
       it "updates the setting" do
         @setting.should_receive(:update_attribute).with(:value, :new_value)
-        Config.persist('some_method', :new_value)
+        Settings.persist('some_method', :new_value)
       end
     end
 
     context "a value that is not persisted" do
       it "creates the setting" do
-        Config.stub_chain(:where, :last).and_return(nil)
-        Config.should_receive(:create).with(:var => 'new_method', :value => 'value')
-        Config.persist('new_method', 'value')
+        Settings.stub_chain(:where, :last).and_return(nil)
+        Settings.should_receive(:create).with(:var => 'new_method', :value => 'value')
+        Settings.persist('new_method', 'value')
       end
     end
   end
 
   describe :read_from_persistance do
     it "reads the value from persistance" do
-      object = Config.new :var => 'foo', :value => 'bar'
-      Config.should_receive(:find_by_var).with('foo').and_return(object)
+      object = Settings.new :var => 'foo', :value => 'bar'
+      Settings.should_receive(:find_by_var).with('foo').and_return(object)
 
-      Config.read_from_persistance('foo').should eq 'bar'
+      Settings.read_from_persistance('foo').should eq 'bar'
     end
   end
 
   describe :cache_key_for do
     it "returns the name for the rails cache key" do
-      Config.cache_key_for('foo').should eq('settings/foo')
+      Settings.cache_key_for('foo').should eq('settings/foo')
     end
   end
 
@@ -111,20 +105,20 @@ describe Persistent::Settings do
 
     before do
       Rails.should_receive(:cache).and_return cache
-      Config.stub(:cache_key_for).with('foo').and_return 'settings/foo'
+      Settings.stub(:cache_key_for).with('foo').and_return 'settings/foo'
     end
 
     describe :write_to_cache do
       it "writes the key, value to Rails cache" do
         cache.should_receive(:write).with('settings/foo', 'bar')
-        Config.write_to_cache('foo', 'bar')
+        Settings.write_to_cache('foo', 'bar')
       end
     end
 
     describe :read_from_cache do
       it "reads the value from Rails cache" do
         cache.should_receive(:fetch).with('settings/foo')
-        Config.read_from_cache('foo')
+        Settings.read_from_cache('foo')
       end
     end
   end
@@ -132,29 +126,29 @@ describe Persistent::Settings do
   describe :method_missing do
     context "A method ending with =" do
       it "calls define_setter_and_getter" do
-        Config.should_receive(:define_setter_and_getter).with(:new_method=)
-        Config.should_receive(:new_method=).with('value')
-        Config.method_missing(:new_method=, 'value')
+        Settings.should_receive(:define_setter_and_getter).with(:new_method=)
+        Settings.should_receive(:new_method=).with('value')
+        Settings.method_missing(:new_method=, 'value')
       end
     end
 
     context "method that does not end with =" do
       context "already persisted" do
         before :each do
-          @setting = mock(Config, :var => 'persisted_method', :value => 'some value')
-          Config.stub(:all).and_return([@setting])
+          @setting = mock(Settings, :var => 'persisted_method', :value => 'some value')
+          Settings.stub(:all).and_return([@setting])
         end
 
         it "pulls the value from the database and creates its setter and getter" do
-          Config.should_receive(:persisted_method=).with('some value')
-          Config.load_from_persistance!
+          Settings.should_receive(:persisted_method=).with('some value')
+          Settings.load_from_persistance!
         end
       end
 
       context "not persisited" do
         it "blows up" do
           lambda {
-            Config.just_a_method
+            Settings.just_a_method
           }.should raise_error(NoMethodError)
         end
       end
@@ -167,10 +161,10 @@ describe Persistent::Settings do
     end
 
     before do
-      Config.should_receive(:select).with(:var).and_return(settings)
+      Settings.should_receive(:select).with(:var).and_return(settings)
     end
 
-    subject { Config.keys }
+    subject { Settings.keys }
 
     specify { should include(:a_key) }
     specify { should include(:another_key) }
@@ -178,34 +172,34 @@ describe Persistent::Settings do
 
   describe :load_from_persistance! do
     it "loads all settings from persistance and sets it up" do
-      settings = [ Config.new(:var => 'foo', :value => 'bar') ]
-      Config.should_receive(:all).and_return(settings)
-      Config.should_receive("foo=").with('bar')
+      settings = [ Settings.new(:var => 'foo', :value => 'bar') ]
+      Settings.should_receive(:all).and_return(settings)
+      Settings.should_receive("foo=").with('bar')
 
-      Config.load_from_persistance!
+      Settings.load_from_persistance!
     end
   end
 
   describe :load_from_persistance do
     context "is ready" do
       before do
-        Config.stub(:ready?).and_return true
+        Settings.stub(:ready?).and_return true
       end
 
       it "calls load_from_persistance!" do
-        Config.should_receive(:load_from_persistance!)
-        Config.load_from_persistance
+        Settings.should_receive(:load_from_persistance!)
+        Settings.load_from_persistance
       end
     end
 
     context "not ready" do
       before do
-        Config.stub(:ready?).and_return false
+        Settings.stub(:ready?).and_return false
       end
 
       it "calls load_from_persistance!" do
-        Config.should_not_receive(:load_from_persistance!)
-        Config.load_from_persistance
+        Settings.should_not_receive(:load_from_persistance!)
+        Settings.load_from_persistance
       end
     end
   end
@@ -213,34 +207,34 @@ describe Persistent::Settings do
   describe :ready? do
     context "AR is connected and table is created" do
       before do
-        Config.stub(:connected?).and_return true
-        Config.stub(:table_exists?).and_return true
+        Settings.stub(:connected?).and_return true
+        Settings.stub(:table_exists?).and_return true
       end
 
       it "is true" do
-        Config.ready?.should be_true
+        Settings.ready?.should be_true
       end
     end
 
     context "AR is not connected" do
       before do
-        Config.stub(:connected?).and_return false
-        Config.stub(:table_exists?).and_return true
+        Settings.stub(:connected?).and_return false
+        Settings.stub(:table_exists?).and_return true
       end
 
       it "is false" do
-        Config.ready?.should be_false
+        Settings.ready?.should be_false
       end
     end
 
     context "table is not created" do
       before do
-        Config.stub(:connected?).and_return false
-        Config.stub(:table_exists?).and_return false
+        Settings.stub(:connected?).and_return false
+        Settings.stub(:table_exists?).and_return false
       end
 
       it "is false" do
-        Config.ready?.should be_false
+        Settings.ready?.should be_false
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,15 @@ ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new("test.log")
 ::ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
+Dir["./spec/support/**/*.rb"].each {|f| require f}
+
 ::ActiveRecord::Schema.define(:version => 1) do
-  create_table "settings", :force => true do |t|
+  create_table "configs", :force => true do |t|
     t.string "var"
     t.string "value"
   end
 end
 
-class Settings < ActiveRecord::Base
-  include Persistent::Settings
-end
+#class Settings < ActiveRecord::Base
+  #include Persistent::Settings
+#end
